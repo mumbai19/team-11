4750751beb7854266c548910ea2f8ad16782179f
Kushal Doshi <31789622+Kushald2805@users.noreply.github.com>
2019-07-20 20:55:46 +0530

    Update navbar.php

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -12,25 +12,25 @@
         </button>
         <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
           <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-            <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
             <li class="nav-item submenu dropdown">
               <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                 aria-expanded="false">Shop</a>
-              <ul class="dropdown-menu">
+              <!--<ul class="dropdown-menu">
                 <li class="nav-item"><a class="nav-link" href="category.html">Shop Category</a></li>
                 <li class="nav-item"><a class="nav-link" href="single-product.html">Product Details</a></li>
                 <li class="nav-item"><a class="nav-link" href="checkout.html">Product Checkout</a></li>
                 <li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
                 <li class="nav-item"><a class="nav-link" href="cart.html">Shopping Cart</a></li>
-              </ul>
+              </ul>-->
             </li>
             <li class="nav-item submenu dropdown">
               <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                 aria-expanded="false">Donate</a>
-              <ul class="dropdown-menu">
+              <!--<ul class="dropdown-menu">
                 <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
                 <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-              </ul>
+              </ul>-->
             </li>
               <!-- <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                 aria-expanded="false">Pages</a>
