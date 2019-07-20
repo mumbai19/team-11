8266118052799835665605efb8efc24271996951
SahilSahu <2016.sahil.sahu@ves.ac.in>
2019-07-20 07:05:43 -0700

    Login to be validated and functioned

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -90,13 +90,14 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 
 
 
-  <script src="..../vendors/jquery/jquery-3.2.1.min.js"></script>
-  <script src="..../vendors/bootstrap/bootstrap.bundle.min.js"></script>
-  <script src="..../vendors/skrollr.min.js"></script>
-  <script src="..../vendors/owl-carousel/owl.carousel.min.js"></script>
-  <script src="..../vendors/nice-select/jquery.nice-select.min.js"></script>
-  <script src="..../vendors/jquery.ajaxchimp.min.js"></script>
-  <script src="..../vendors/mail-script.js"></script>
+  <script src="../vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="../vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="../vendors/skrollr.min.js"></script>
+  <script src="../vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="../vendors/nice-select/jquery.nice-select.min.js"></script>
+  <script src="../vendors/jquery.ajaxchimp.min.js"></script>
+  <script src="../vendors/mail-script.js"></script>
   <script src="../js/main.js"></script>
+  <script>
 </body>
 </html>
